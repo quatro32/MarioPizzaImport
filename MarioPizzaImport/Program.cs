0989e0aef8c79418cb9520d0f1cdef7ead340033
NickvandeGroes <n.vandegroes@gmail.com>
2021-03-22 20:42:37 +0100

    Call the importer.

--- MarioPizzaImport/Program.cs
+++ MarioPizzaImport/Program.cs
@@ -141,9 +141,8 @@ namespace MarioPizzaImport
             IngredientImporter ingredientImporter = new IngredientImporter(database, countrycode);
             ingredientImporter.Run(@"C:\Users\Peter\Downloads\MarioData\pizza_ingredienten.csv");
 
-            //InsertBottoms(@"C:\Users\Peter\Downloads\MarioData\pizzabodems.csv", database, countrycode);
-            ProductImporter productImporter = new ProductImporter(database, countrycode);
-            productImporter.Run(@"C:\Users\Peter\Downloads\MarioData\overige producten.csv");
+            ExtraIngredientImporter extraIngredientImporter = new ExtraIngredientImporter(database, countrycode);
+            extraIngredientImporter.Run(@"C:\Users\Peter\Downloads\MarioData\Extra Ingredienten.csv");
 
             /**
              * Once all other changes are merged, we need to make this into a proper console application with multiple commands. Since the mapping should
@@ -157,104 +156,9 @@ namespace MarioPizzaImport
             Console.ReadKey();
         }
 
-        static void InsertBottoms(string path, dbi298845_prangersEntities db, countrycode countrycode)
-        {
-
-            using (StreamReader sr = new StreamReader(path))
-            {
-                String line;
-                bool isHeaderLine = true;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    if (isHeaderLine)//skip first line in csv file, since it's an header line. we don't want that values
-                    {
-                        isHeaderLine = false;
-                    }
-                    else
-                    {
-                        //split all lines in csv to values
-                        string[] parts = line.Split(';');
-                        string name = parts[0];
-                        string diameter = parts[1];
-                        string description = parts[2];
-                        Decimal price = Decimal.Parse(Regex.Replace(parts[3], "[^0-9.]", ""));
-
-                        //check if bottom exists by it's name
-                        var bottom = db.bottoms.SingleOrDefault(i => i.name == name);
-                        if (bottom == null)//if not, create a new one
-                        {
-                            bottom = new bottom()
-                            {
-                                name = name,
-                                diameter = Convert.ToInt32(diameter),
-                                //description has to be added to database,
-                            };
-
-                            //if a bottom doesn't exists ALWAYS create a bottomprice, since it hasn't got any
-                            var bottomprice = new bottomprice()
-                            {
-                                bottom = bottom,
-                                countrycode = countrycode,
-                                currency = "EUR",
-                                startdate = DateTime.Now,
-                                vat = 9.0m,
-                                price = price
-                            };
-
-                            db.bottomprices.Add(bottomprice);
-                            db.SaveChanges();
-                            Console.WriteLine("1 bottom and bottomprice added...");
-                        }
-                    }
-                }
-            }
-        }
-    
         static void InsertExtraIngredients(string path, dbi298845_prangersEntities db, countrycode countrycode)
         {
-            using (StreamReader sr = new StreamReader(path))
-            {
-                String line;
-                bool isHeaderLine = true;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    if (isHeaderLine)
-                    {
-                        isHeaderLine = false;
-                    }
-                    else
-                    {
-                        string[] parts = line.Split(';');
-                        string name = parts[0];
-                        decimal price = Decimal.Parse(Regex.Match(parts[1], @"[0-9]+(\.[0-9]+)?").Value);
-
-                        // Controleren of het ingredient al voorkomt
-                        var ingredient = db.ingredients.SingleOrDefault(i => i.name == name);
-                        if (ingredient == null)
-                        {
-                            ingredient = new ingredient()
-                            {
-                                name = name
-                            };
-
-                            var ingredientprice = new ingredientprice()
-                            {
-                                ingredient = ingredient,
-                                startdate = DateTime.Now,
-                                vat = 9.0m,
-                                price = price,
-                                currency = "EUR",
-                                countrycode = countrycode
-                            };
-
-                            ingredient.ingredientprices.Add(ingredientprice);
-                            db.ingredients.Add(ingredient);
-                            db.SaveChanges();
-                            Console.WriteLine("Ingredient added");
-                        }
-                    }
-                }
-            }
+            
         }
 
         private static countrycode getOrCreateDefaultCountryCode(dbi298845_prangersEntities db)
